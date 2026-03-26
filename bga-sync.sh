211a81e157224cad03cc826deb8bc8b26608a3ed
quietmint <quietmint@users.noreply.github.com>
2026-03-26 18:57:46 -0400

    Initial commit

---
+++ bga-sync.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -ex
+#cd src
+#[[ $1 == "css" ]] && sass --style compressed --no-source-map index.scss ../nowboarding.css
+#[[ $1 == 'js' ]] && npx babel index.js --out-file ../nowboarding.js
+#cd ..
+bga wildgardens
